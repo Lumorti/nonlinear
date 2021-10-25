10f981f765502e1a2ecca46700bbdcf36d88b6b9
Lumorti <lbmortimer1998@gmail.com>
2021-10-25 20:35:51 +0200

    the search continues

---
+++ relaxTest/prettyPrint.h
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+// Settings
+const int precision = 3;
+
+// Pretty print a generic 1D vector
+template <typename type> void prettyPrint(std::string pre, std::vector<type> arr) {
+
+	// Used fixed precision
+	std::cout << std::fixed << std::showpos << std::setprecision(precision);
+
+	// For the first line, add the pre text
+	std::cout << pre << " { ";
+
+	// For the x values, combine them all on one line
+	for (int x=0; x<arr.size(); x++) {
+		std::cout << arr[x];
+		if (x < arr.size()-1) {
+			std::cout << ", ";
+		}
+	}
+
+	// Output the row
+	std::cout << "}" << std::endl;
+
+	// Reset things for normal output
+	std::cout << std::noshowpos;
+
+}
+
+// Pretty print a 1D dense Eigen array
+template <typename type> void prettyPrint(std::string pre, Eigen::Matrix<type, -1, 1> arr) {
+
+	// Used fixed precision
+	std::cout << std::fixed << std::showpos << std::setprecision(precision);
+
+	// Spacing
+	std::cout << pre << " { ";
+
+	// For the x values, combine them all on one line
+	for (int x=0; x<arr.rows(); x++) {
+		std::cout << std::setw(5) << arr(x);
+		if (x < arr.rows()-1) {
+			std::cout << ", ";
+		}
+	}
+
+	// Output the row
+	std::cout << "}" << std::endl;
+
+	// Reset things for normal output
+	std::cout << std::noshowpos;
+
+}
+
+// Pretty print a general 2D dense Eigen array
+template <typename type> void prettyPrint(std::string pre, Eigen::Matrix<type, -1, -1> arr) {
+
+	// Used fixed precision
+	std::cout << std::fixed << std::showpos << std::setprecision(precision);
+
+	// Loop over the array
+	std::string rowText;
+	for (int y=0; y<arr.rows(); y++) {
+
+		// For the first line, add the pre text
+		if (y == 0) {
+			rowText = pre + "{";
+
+		// Otherwise pad accordingly
+		} else {
+			rowText = "";
+			while (rowText.length() < pre.length()+1) {
+				rowText += " ";
+			}
+		}
+
+		// Spacing
+		std::cout << rowText << " { ";
+
+		// For the x values, combine them all on one line
+		for (int x=0; x<arr.cols(); x++) {
+			std::cout << std::setw(precision+2) << arr(y,x);
+			if (x < arr.cols()-1) {
+				std::cout << ", ";
+			}
+		}
+
+		// Output the row
+		std::cout << "}";
+		if (y < arr.rows()-1) {
+			std::cout << ",";
+		} else {
+			std::cout << " } ";
+		}
+		std::cout << std::endl;
+
+	}
+
+	// Reset things for normal output
+	std::cout << std::noshowpos;
+
+}
+
+// Pretty print a general 2D sparse Eigen array
+template <typename type> void prettyPrint(std::string pre, Eigen::SparseMatrix<type> arr) {
+
+	// Extract the dense array and then call the routine as normal
+	prettyPrint(pre, Eigen::Matrix<type,-1,-1>(arr));
+
+}
+
+// Pretty print a double
+void prettyPrint(std::string pre, double val) {
+	std::cout << std::scientific << std::setprecision(precision) << pre << val << std::endl;
+}
+
+// Pretty print a complex
+void prettyPrint(std::string pre, std::complex<double> val) {
+	std::cout << std::fixed << std::showpos << std::setprecision(precision) << pre << val.real() << val.imag() << "i" << std::endl;
+	std::cout << std::noshowpos;
+}
